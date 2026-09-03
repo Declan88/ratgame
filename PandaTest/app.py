0875053132d3d0a3eb45ab29763a2e9ad987a6df
MonkeAlt <monkealternate@gmail.com>
2026-09-03 13:07:21 -0500

    delete test

--- PandaTest/app.py
+++
@@ -1,9 +0,0 @@
-from direct.showbase.ShowBase import ShowBase
-app=ShowBase()
-
-app.set_background_color(0,0,0,1)
-app.cam.setPos(0, -5, 0)
-model = app.loader.load_model("models/cylinder")
-model.reparent_to(app.render)
-
-app.run()
